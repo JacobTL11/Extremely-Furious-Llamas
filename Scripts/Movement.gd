4a5d78ac514dd4699cae63174234b82e0d57bd65
JacobTL11 <jtlibertoski@gmail.com>
2022-09-16 14:41:14 -0500

    Rigged flip in movement

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-onready var sprite = get_node("Charlles")
+onready var Charlles = get_node("Charlles")
 var speed : int = 600
-var jump_speed : int = 400
+var jump_speed : int = 500
 var gravity : int = 1000
 var velocity = Vector2()
 
@@ -10,12 +10,12 @@ func get_input(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= speed
-		if (sprite.scale.x == 1):
-			sprite.scale.x = -1
+		if (Charlles.scale.x == 1):
+			Charlles.scale.x = -1
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
-		if (sprite.scale.x == -1):
-			sprite.scale.x = 1
+		if (Charlles.scale.x == -1):
+			Charlles.scale.x = 1
 	if Input.is_action_pressed("jump"):
 		if (is_on_floor()):
 			velocity.y -= jump_speed
