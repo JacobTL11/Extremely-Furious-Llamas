e1132e53329eff59ccd1c038746ca3371fdf5fe3
JacobTL11 <jtlibertoski@gmail.com>
2022-09-21 13:11:00 -0500

    Player animates when they move

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -6,28 +6,35 @@ var speed = 600
 var jump_speed = 500
 var gravity = 1000
 var velocity = Vector2()
-
-func _ready():
-	animator.play("Idle")
-	pass
+var idle = true
 
 func get_input(delta):
+	idle = true
 	velocity.x = 0
 	if Input.is_action_pressed("move_left"):
+		idle = false
+		animator.play("Walk")
 		velocity.x -= speed
 		if (Charlles.scale.x == 1):
 			Charlles.scale.x = -1
 	if Input.is_action_pressed("move_right"):
+		idle = false
+		animator.play("Walk")
 		velocity.x += speed
 		if (Charlles.scale.x == -1):
 			Charlles.scale.x = 1
 	if Input.is_action_pressed("jump"):
+		idle = false
+		animator.play("Jump")
 		if (is_on_floor()):
 			velocity.y -= jump_speed
 
 	#gravity
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if(idle && is_on_floor()):
+		animator.play("Idle")
 	pass
 
 func _physics_process(delta):
