250371142632229ddd5fd0d9f77c00b2b90cc027
Alex Mohr <Nintencraft360@gmail.com>
2022-09-26 13:09:21 -0500

    meat count

--- Scripts/Knife.gd
+++ Scripts/Knife.gd
@@ -4,3 +4,4 @@ func _physics_process(_delta):
 	for body in get_overlapping_bodies():
 		if body.get_name() == "Human":
 			body.get_node("CollisionShape2D").die()
+			self.get_parent().pickup_meat()
