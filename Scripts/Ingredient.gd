c38c1051f90e204cc1728cb8602d8a1778e9090a
Alex Mohr <Nintencraft360@gmail.com>
2022-09-28 13:32:20 -0500

    Carrot auido doesnt work

--- Scripts/Ingredient.gd
+++ Scripts/Ingredient.gd
@@ -4,5 +4,9 @@ func _physics_process(delta):
 	for body in get_overlapping_bodies():
 		if body.name == "Player":
 			Collectables.pickup_ingredient(self.get_parent().name)
-			#gt audio stream and play pop sound
+			print(get_parent().get_node("PickupSound").name)
+			var effect = get_parent().get_node("PickupSound")
+			effect.play()
+			print(effect.name)
+			#yield(get_tree().create_timer(0.5), "timeout")
 			self.get_parent().queue_free()
