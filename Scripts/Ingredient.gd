0f4ed2eb11c4d1b7bcce7a66593766b755a18a1e
Alex Mohr <Nintencraft360@gmail.com>
2022-09-28 14:22:41 -0500

    testing

--- Scripts/Ingredient.gd
+++ Scripts/Ingredient.gd
@@ -1,12 +1,24 @@
 extends Area2D
-
+onready var effect = get_parent().get_node("PickupSound")
 func _physics_process(delta):
 	for body in get_overlapping_bodies():
 		if body.name == "Player":
 			Collectables.pickup_ingredient(self.get_parent().name)
-			print(get_parent().get_node("PickupSound").name)
-			var effect = get_parent().get_node("PickupSound")
+			#print(get_parent().get_parent().get_node("PickupSound").name)
+			
+			
 			effect.play()
+			print(effect.is_playing())
 			print(effect.name)
 			#yield(get_tree().create_timer(0.5), "timeout")
 			self.get_parent().queue_free()
+			
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var effect = get_parent().get_node("PickupSound")
+	#effect.play() # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
