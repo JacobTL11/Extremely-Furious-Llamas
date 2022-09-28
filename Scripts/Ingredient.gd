9a33cc8402675d8d531e7df72f125f9eb4f7db3f
Alex Mohr <Nintencraft360@gmail.com>
2022-09-28 13:06:53 -0500

    add llama count and place to add ingredient pickup sound

--- Scripts/Ingredient.gd
+++ Scripts/Ingredient.gd
@@ -4,4 +4,5 @@ func _physics_process(delta):
 	for body in get_overlapping_bodies():
 		if body.name == "Player":
 			Collectables.pickup_ingredient(self.get_parent().name)
+			#gt audio stream and play pop sound
 			self.get_parent().queue_free()
--- Scripts/Knife.gd
+++ Scripts/Knife.gd
@@ -4,6 +4,7 @@ func _physics_process(_delta):
 	for body in get_overlapping_bodies():
 		if body.get_name() == "Human" || body.get_name() == "Llama" :
 			body.get_node("CollisionShape2D").die()
-			self.get_parent().get_parent().pickup_human_meat()
-			
-#when we add llamas add similar pickup func call here
+			if body.get_name() == "Human":
+				self.get_parent().get_parent().pickup_human_meat()
+			if body.get_name() == "Llama":
+				self.get_parent().get_parent().pickup_llama_meat()
