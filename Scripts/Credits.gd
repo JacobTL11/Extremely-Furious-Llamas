f0dfede53bed7149991efca2d29bf8708bd57855
Alex Mohr <Nintencraft360@gmail.com>
2022-09-28 19:41:29 -0500

    ending credits and reload main menu with space

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -7,8 +7,9 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _input(ev):
+	if Input.is_key_pressed(KEY_SPACE):
+		get_tree().change_scene("res://Scenes/Menu.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
