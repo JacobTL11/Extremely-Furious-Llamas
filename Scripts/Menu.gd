7d7ae3d192a0d59dde114ead88e02eb3ab2a35e0
JacobTL11 <jtlibertoski@gmail.com>
2022-09-28 19:39:39 -0500

    main menu and fixes

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/JacobTestScene.tscn")
+	get_tree().change_scene("res://Scenes/Main Level.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
